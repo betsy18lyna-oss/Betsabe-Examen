c5c380ec11132f921cfa0362cd3a2912e749c7de
betsy18lyna-oss <betsy18lyna@gmail.com>
2025-09-16 09:39:02 -0500

    bbbb
    desplegar

--- cine_betsa/peliculas/views.py
+++ cine_betsa/peliculas/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login, authenticate
-from .forms import RegistrarUsuarioForm
-from .forms import PeliculaForm
+from django.contrib.auth import login
+from .forms import RegistrarUsuarioForm, PeliculaForm
 from .models import Pelicula
 from django.contrib.auth.decorators import login_required
+
 
 def registrar_usuario(request):
     if request.method == 'POST':
@@ -17,6 +17,7 @@ def registrar_usuario(request):
         form = RegistrarUsuarioForm()
     return render(request, 'registrar.html', {'form': form})
 
+
 def login_usuario(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
@@ -28,18 +29,23 @@ def login_usuario(request):
         form = AuthenticationForm()
     return render(request, 'Login.html', {'form': form})
 
-    @login_required
-    def registrar_pelicula(request):
-        if request.method == 'POST':
-            form = PeliculaForm(request.POST)
-            if form.is_valid():
-                form.save()
-                return redirect('listaspeliculas')
-            else:
-                form = PeliculaForm()
-                return render(request, 'registrar_pelicula.html', {'form': form})
-    def lista_peliculas(request):
-        peliculas = Pelicula.objects.all()
-        return render(request, 'listaspeliculas.html', {'peliculas': peliculas})
+
+@login_required
+def registrar_pelicula(request):
+    if request.method == 'POST':
+        form = PeliculaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listaspeliculas')
+    else:
+        form = PeliculaForm()
+    return render(request, 'registrar_pelicula.html', {'form': form})
+
+
+@login_required
+def lista_peliculas(request):
+    peliculas = Pelicula.objects.all()
+    return render(request, 'listaspeliculas.html', {'peliculas': peliculas})
+
 
 
